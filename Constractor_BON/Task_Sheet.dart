caf0dfae33f54eeb806aacc5d27f87b616cfea82
ramsabah <ramsabah28@gmail.com>
2025-06-26 11:11:59 +0200

    Task_Sheet_A2_A3

--- Constractor_BON/Task_Sheet.dart
+++ Constractor_BON/Task_Sheet.dart
@@ -12,21 +12,39 @@ class Button {
   final int padding;
 
   Button(
-      this.titel,
-      this.width,
-      this.height,
-      this.radius,
-      this.fontSize,
-      this.padding,
-      );
+    this.titel,
+    this.width,
+    this.height,
+    this.radius,
+    this.fontSize,
+    this.padding,
+  );
+}
+
+class Rect {
+  final int y;
+  final int x;
+
+  Rect({required this.y, required this.x});
+
+  int calculatePerimeter() {
+    return 2 * (this.x + this.y);
+  }
+
+  int calculateSpace() {
+    return this.y * this.x;
+  }
 }
 
 void main() {
   print(Person.name);
   print(Person.age);
 
-
   Button b = Button("In den Warenkorp", 96, 32, 4, 14, 8);
 
-  print("Button: ${b.titel}, ${b.width}, ${b.height}, ${b.radius}, ${b.fontSize}, ${b.padding}");
+  print(
+      "Button: ${b.titel}, ${b.width}, ${b.height}, ${b.radius}, ${b.fontSize}, ${b.padding}");
+  Rect r = Rect(y: 5, x: 6);
+  print(r.calculatePerimeter());
+  print(r.calculateSpace());
 }
